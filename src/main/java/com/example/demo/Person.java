2e32f442d748b2c9ac59e61c50b32d52ae2decbb
MarcoVegaD <zerosnipo@gmail.com>
2022-01-23 13:40:43 -0600

    first commit

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String nombre;
+    private String apellido;
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    
+}
